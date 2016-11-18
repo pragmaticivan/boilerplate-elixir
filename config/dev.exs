f2473753c0de0517a6dececad2376330cc044369
Ivan Santos <pragmaticivan@gmail.com>
2016-11-18 11:52:01 -0800

    Update start hook

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :boilerplate_phoenix, BoilerplatePhoenix.Endpoint,
-  http: [port: 80],
+  http: [port: System.get_env("PORT") || Application.get_env(:phoenix, :port) || 80],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
